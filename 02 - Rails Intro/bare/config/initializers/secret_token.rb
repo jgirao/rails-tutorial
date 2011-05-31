ae21735b4a7f27496c216524dc6237404af2e7b4
Joao Girao <joao@girao.org>
2011-05-31 03:07:26 +0200

    added bare project

---
+++ 02 - Rails Intro/bare/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bare::Application.config.secret_token = '18be625b82488f34711ede81c3a26952761476986d647854a4b1a2448c1dc0cd74e03878faab9e525e11bde5d6df8c5c5e535970769ad1ae686bffab69cdf4af'
